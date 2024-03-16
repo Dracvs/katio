cb18a36ae4106f5046174f6b7b7ddb4faf748876
Luis Robles <luisr@mill5.com>
2024-03-16 08:55:10 -0500

    Creado entidad y controlador para los libros.

---
+++ src/main/java/edu/eafit/katio/controllers/BooksController.java
@@ -0,0 +1,19 @@
+package edu.eafit.katio.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.eafit.katio.models.Book;
+
+@RestController
+@RequestMapping("/katio/books")
+public class BooksController {
+    
+    @GetMapping("/getall")
+    public ResponseEntity<Book> getAll(){
+        return new ResponseEntity<Book>(new Book(), HttpStatus.OK);
+    }
+}
