0cb3b69ad1e1b9356475f4aa306acf09b37b6d98
Luis Robles <Dracvs@users.noreply.github.com>
2024-03-23 08:57:08 -0500

    Bulk Insert of Books.

--- src/main/resources/static/database_creation.sql
+++ src/main/resources/static/database_creation.sql
@@ -54,4 +54,7 @@ INSERT INTO Authors
     VALUES ('Germán', 'Castro-Caycedo', 'Colombia', '1940-03-03');
 
 
-
+INSERT INTO Books VALUES 
+    (0, 'Cien años de soledad', '8420471836', '978-8420471839', '1967-06-05', 'RAE Obra Académica', 'Realismo Mágico', '800', 1),
+    (0, 'Huellas', '9584277278', '978-958427275', '2019-01-01', '1ra edicion', 'Crónica', '800', 3),
+    (0, 'María', '14802722922', '978-148027292', '1867-01-01', '1ra edicion', 'Romancticismo', '800', 2)
